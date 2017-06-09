9f00dd08783449cfdb023a926662b65351e1bac2
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-09 19:51:28 +0300

    Upload solutions.

---
+++ 08. ProgrammingBasicsExam20112016Evening/01. HousePrice/01. HousePrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double smallestRoomArea = double.Parse(Console.ReadLine());
+        double kitchenArea = double.Parse(Console.ReadLine());
+        double priceKvM = double.Parse(Console.ReadLine());
+
+        var secondRoom = smallestRoomArea + (smallestRoomArea * 0.10);
+        var thirdRoom = secondRoom + (secondRoom * 0.10);
+        var bathroom = smallestRoomArea / 2;
+        var totalArea = smallestRoomArea + kitchenArea + secondRoom + thirdRoom + bathroom;
+        totalArea = totalArea + (totalArea * 0.05);
+
+        Console.WriteLine("{0:f2}", totalArea*priceKvM);
+    }
+}
---
+++ 08. ProgrammingBasicsExam20112016Evening/03. BikeRace/03. BikeRace.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int juniorsNum = int.Parse(Console.ReadLine());
+        int seniorsNum = int.Parse(Console.ReadLine());
+        string layout = Console.ReadLine().ToLower();
+
+        var totalSum = 0.00;
+
+        if (layout == "trail")
+        {
+            totalSum = juniorsNum * 5.50 + seniorsNum * 7;
+        }
+        else if (layout == "cross-country")
+        {
+            if ((juniorsNum + seniorsNum) >= 50)
+            {
+                totalSum = juniorsNum * 8 + seniorsNum * 9.50;
+                totalSum = totalSum - (totalSum * 0.25);
+            }
+            else
+            {
+                totalSum = juniorsNum * 8 + seniorsNum * 9.50;
+            }
+        }
+        else if (layout == "downhill")
+        {
+            totalSum = juniorsNum * 12.25 + seniorsNum * 13.75;
+        }
+        else if (layout == "road")
+        {
+            totalSum = juniorsNum * 20 + seniorsNum * 21.50;
+        }
+
+        var expenses = totalSum * 0.05;
+
+        Console.WriteLine("{0:f2}", Math.Abs(totalSum - expenses));
+    }
+}
