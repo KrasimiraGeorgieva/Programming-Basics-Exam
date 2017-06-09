9f17442d9f778f07ccfdfdcd9355c4fcab9073fb
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-09 19:40:17 +0300

    Upload solutions.

---
+++ 08. ProgrammingBasicsExam20112016Morning/01. Fishland/01. Fishland.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skumriaPrice = double.Parse(Console.ReadLine());
+            double cacaPrice = double.Parse(Console.ReadLine());
+            double palamudKg = double.Parse(Console.ReadLine());
+            double safridKg = double.Parse(Console.ReadLine());
+            int midiKg = int.Parse(Console.ReadLine());
+
+            double palamudPrice = skumriaPrice + skumriaPrice * 0.60;
+            double safridPrice = cacaPrice + cacaPrice * 0.80;
+            double midiPrice = 7.50;
+
+            double palamud = palamudKg * palamudPrice;
+            double safrid = safridKg * safridPrice;
+            double midi = midiKg * midiPrice;
+
+            Console.WriteLine("{0:f2}", palamud+safrid+midi);
+         }
+    }
+}
+
---
+++ 08. ProgrammingBasicsExam20112016Morning/04. SoftUni Camp/04. SoftUni Camp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _04.SoftUni_Camp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupNum = int.Parse(Console.ReadLine());
+
+            int peopleNumInGroup=0;
+            int carPeople = 0;
+            int minibusPeople = 0;
+            int smallBusPeople = 0;
+            int bigBusPeople = 0;
+            int trainPeople = 0;
+            int totalPeople = 0;
+
+            for (int i = 1; i <= groupNum; i++)
+            {
+                peopleNumInGroup = int.Parse(Console.ReadLine());
+                totalPeople += peopleNumInGroup;
+                if (peopleNumInGroup <= 5)
+                {
+                    carPeople += peopleNumInGroup;
+                }
+                else if (peopleNumInGroup >= 6 && peopleNumInGroup <= 12)
+                {
+                    minibusPeople += peopleNumInGroup;
+                }
+                else if (peopleNumInGroup >= 13 && peopleNumInGroup <= 25)
+                {
+                    smallBusPeople += peopleNumInGroup;
+                }
+                else if (peopleNumInGroup >= 26 && peopleNumInGroup <= 40)
+                {
+                    bigBusPeople += peopleNumInGroup;
+                }
+                else if (peopleNumInGroup >= 41)
+                {
+                    trainPeople += peopleNumInGroup;
+                }
+            }
+            var result = totalPeople / 100.0;
+            Console.WriteLine("{0:F2}%", carPeople / result);
+            Console.WriteLine("{0:F2}%", minibusPeople / result);
+            Console.WriteLine("{0:F2}%", smallBusPeople / result);
+            Console.WriteLine("{0:F2}%", bigBusPeople /result);
+            Console.WriteLine("{0:F2}%", trainPeople / result);
+        }
+    }
+}
