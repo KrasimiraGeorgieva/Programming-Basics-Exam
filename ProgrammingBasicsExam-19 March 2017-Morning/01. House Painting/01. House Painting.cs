c91454c1cfb658e260f95131bb1f5b4667087637
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-20 18:46:09 +0300

    Upload 01.House Painting Solution.

---
+++ ProgrammingBasicsExam-19 March 2017-Morning/01. House Painting/01. House Painting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double houseHeight = double.Parse(Console.ReadLine());
+        double wallLenght = double.Parse(Console.ReadLine());
+        double roofHeight = double.Parse(Console.ReadLine());
+
+        //Walls
+        var wall = houseHeight * wallLenght;
+        var window = 1.5 * 1.5;
+        var wallsLeftRight = 2 * wall - 2 * window;
+        var wallBackFront = houseHeight * houseHeight;
+        var door = 1.2 * 2;
+        var totalFrontBack = 2 * wallBackFront - door;
+        var totalBehindInFront = wallsLeftRight + totalFrontBack;
+        var greenPaint = totalBehindInFront / 3.4;
+
+        //Roof
+        var rectangle2 = 2 * (houseHeight * wallLenght);
+        var triangle2 = 2 * ((houseHeight * roofHeight) / 2);
+        var totalRoof = rectangle2 + triangle2;
+        var redPaint = totalRoof / 4.3;
+
+        Console.WriteLine("{0:f2}", greenPaint);
+        Console.WriteLine("{0:f2}", redPaint);
+    }
+}
