9cb26b67f130305f58d0db49d4a2f9d716c474ca
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-20 18:47:16 +0300

    Upload 02. Cups solution.

---
+++ ProgrammingBasicsExam-19 March 2017-Morning/02. Cups/02. Cups.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int glassesNums = int.Parse(Console.ReadLine());
+        int workersNums = int.Parse(Console.ReadLine());
+        int workingDays = int.Parse(Console.ReadLine());
+
+        var workingHours = workersNums * workingDays * 8;
+        var glassesMade = Math.Floor(workingHours / 5.0);
+
+        var difference = Math.Abs(glassesNums - glassesMade);
+
+        if (glassesNums < glassesMade)
+        {
+            Console.WriteLine("{0:f2} extra money", (difference * 4.2));
+        }
+        else if(glassesMade < glassesNums)
+        {
+            Console.WriteLine("Losses: {0:f2}", (difference * 4.2));
+        }
+    }
+}
