f8cccee22fd4064085d80c5c388250cff5345a98
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-20 18:52:04 +0300

    Upload 05. Hourglass solution.

---
+++ ProgrammingBasicsExam-19 March 2017-Morning/05. Hourglass/05. Hourglass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var rowLenght = 2 * n + 1;
+
+        Console.WriteLine("{0}", new string('*', 2 * n + 1));
+        Console.WriteLine("{0}*{1}*{0}", new string('.', 1), new string(' ', rowLenght - 4));
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.WriteLine("{0}*{1}*{0}", new string('.', 2 + i), new string('@', rowLenght - 6 - i * 2));
+        }
+
+        Console.WriteLine("{0}*{0}", new string('.', n));
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.WriteLine("{0}*{1}@{1}*{0}", new string('.', n - 1 - i), new string(' ', i));
+        }
+
+        Console.WriteLine("{0}*{1}*{0}", new string('.', 1), new string('@', rowLenght - 4));
+        Console.WriteLine("{0}", new string('*', 2 * n + 1));
+    }
+}
