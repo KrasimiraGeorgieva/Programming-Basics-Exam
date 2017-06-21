5e7b058d2a9b6a06e80289da7403b0f77182fd3e
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-21 15:26:39 +0300

    Add 02.Work hours solution.

---
+++ ProgrammingBasicsExam-18 March 2017/02. Work hours/02. Work hours.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int neededHours = int.Parse(Console.ReadLine());
+        int workermenNum = int.Parse(Console.ReadLine());
+        int workingDays = int.Parse(Console.ReadLine());
+
+        int totalWorkingHours = workermenNum * workingDays * 8;
+
+        if (totalWorkingHours > neededHours)
+        {
+            Console.WriteLine("{0} hours left", totalWorkingHours -neededHours);
+        }
+        else
+        {
+            Console.WriteLine("{0} overtime", neededHours - totalWorkingHours);
+            Console.WriteLine("Penalties: {0}", (neededHours-totalWorkingHours)*workingDays);
+        }
+    }
+}
