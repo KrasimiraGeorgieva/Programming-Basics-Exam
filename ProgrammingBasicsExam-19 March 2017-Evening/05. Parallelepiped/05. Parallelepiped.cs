815fba5a9e3f8024fe8fa4d449f48b0042c080c1
Krasimira Georgieva <krassi77bg@gmail.com>
2017-06-22 15:40:46 +0300

    Add solution.

---
+++ ProgrammingBasicsExam-19 March 2017-Evening/05. Parallelepiped/05. Parallelepiped.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int lenght = 3 * n + 1;
+        Console.WriteLine("+{0}+{1}", new string('~', n - 2), new string('.', 2 * n + 1));
+
+        for (int i = 0; i <= 2 * n; i++)
+        {
+            Console.WriteLine("|{0}\\{1}\\{2}", new string('.', i), new string('~', n - 2), new string('.', lenght - 3 - i - (n - 2)));
+        }
+
+        for (int i = 0; i <= 2 * n; i++)
+        {
+            Console.WriteLine("{0}\\{1}|{2}|", new string('.', i), new string('.', lenght - 3 - i - (n - 2)), new string('~', n - 2));
+        }
+
+
+        Console.WriteLine("{0}+{1}+", new string('.', 2 * n + 1), new string('~', n - 2));
+    }
+}
